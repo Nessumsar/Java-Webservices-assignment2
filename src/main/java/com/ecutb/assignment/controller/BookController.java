fc950efdda25d7241f3cede30c5759847860ac90
Nessumsar <lukasrasmusseen@gmail.com>
2020-10-26 15:35:01 +0100

    CRUD created

--- src/main/java/com/ecutb/assignment/controller/BookController.java
+++ src/main/java/com/ecutb/assignment/controller/BookController.java
@@ -4,7 +4,6 @@ import com.ecutb.assignment.entity.Book;
 import com.ecutb.assignment.service.BookService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,14 +18,55 @@ public class BookController {
 
     @GetMapping
     public List<Book> getAllBooks(){
-        return bookService.getAllBooks();
+        return bookService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable int id) {
+        return bookService.findById(id);
+    }
+
+    @GetMapping("/genre/{genre}")
+    public List<Book> getBooksByGenre(@PathVariable String genre){
+        return bookService.getAllInGenre(genre);
+    }
+
+    @GetMapping("/author/{author}")
+    public List<Book> getBooksByAuthor(@PathVariable String author){
+        return bookService.getAllFromAuthor(author);
+    }
+
+    @GetMapping("/title/{title}")
+    public List<Book> getBooksByTitle(@RequestBody String title){
+        return bookService.getAllByTitle(title);
+    }
+
+    @GetMapping("/isbn/{isbn}")
+    public List<Book> getBooksByIsbn(@RequestBody String isbn){
+        return bookService.getAllByIsbn(isbn);
+    }
+
+    @GetMapping("/available")
+    public List<Book> getBooksByAvailability(){
+        return bookService.getAllAvailable();
+    }
+
+    @PostMapping("/update")
+    public Book updateBook(@RequestBody Book book){
+        return bookService.update(book);
     }
 
     @PostMapping
     public Book saveBook(@RequestBody Book book){
-
-        return bookService.saveBook(book);
+        return bookService.save(book);
     }
+
+    @DeleteMapping("delete/{id}")
+    public void deleteBook(@PathVariable int id){
+        bookService.delete(id);
+    }
+
+
 
 
 
--- src/main/java/com/ecutb/assignment/service/BookService.java
+++ src/main/java/com/ecutb/assignment/service/BookService.java
@@ -17,16 +17,91 @@ public class BookService {
     private final BookRepository bookRepository;
 
 
-    public List<Book> getAllBooks(){
+    public List<Book> getAll(){
         var books = bookRepository.findAll();
         List<Book> result = new ArrayList<>();
         books.forEach(result::add);
         return result;
-
     }
 
+    public List<Book> getAllInGenre(String genre){
+        var books = bookRepository.findAll();
+        List<Book> result = new ArrayList<>();
+        books.forEach(book -> {
+            if (book.getGenre().equals(genre)){
+                result.add(book);
+            }
+        });
+    return result;
+    }
 
-    public Book saveBook(Book book){
+    public List<Book> getAllFromAuthor(String author){
+        var books = bookRepository.findAll();
+        List<Book> result = new ArrayList<>();
+        books.forEach(book -> {
+            if (book.getAuthor().equals(author)){
+                result.add(book);
+            }
+        });
+        return result;
+    }
+
+    public List<Book> getAllAvailable(){
+        var books = bookRepository.findAll();
+        List<Book> result = new ArrayList<>();
+        books.forEach(book -> {
+            if (book.isAvailable()){
+                result.add(book);
+            }
+        });
+        return result;
+    }
+
+    public List<Book> getAllByTitle(String title){
+        var books = bookRepository.findAll();
+        List<Book> result = new ArrayList<>();
+        books.forEach(book -> {
+            if (book.getTitle().equals(title)){
+                result.add(book);
+            }
+        });
+        return result;
+    }
+
+    public List<Book> getAllByIsbn(String isbn){
+        var books = bookRepository.findAll();
+        List<Book> result = new ArrayList<>();
+        books.forEach(book -> {
+            if (book.getIsbn().equals(isbn)){
+                result.add(book);
+            }
+        });
+        return result;
+    }
+
+    public Book findById(int id){
+        if (bookRepository.findById(id).isPresent()){
+            return bookRepository.findById(id).get();
+        }
+        return null;
+    }
+
+    public Book update(Book book){
+        var result = findById(book.getId());
+        if (result != null){
+            return bookRepository.save(book);
+        }
+        return null;
+    }
+
+    public void delete(int id){
+        var result = findById(id);
+        if (result != null){
+            bookRepository.delete(result);
+        }
+    }
+
+    public Book save(Book book){
         return bookRepository.save(book);
     }
 
